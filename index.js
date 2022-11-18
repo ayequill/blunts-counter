9f0ba3f7900d058faf99ee567876de37eab5e743
ayequill <ayequillgh@gmail.com>
2022-11-18 08:33:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let countEl = document.getElementById ("count-el")
+let saveEl = document.getElementById ("save-el")
+let count = 0
+
+function increment() {
+    // you can use += to increase or add to a variable 
+    count += 1
+    countEl.textContent = count
+}
+function save() {
+    let saveEntry =  " " + count + " - "
+    saveEl.textContent += saveEntry
+    countEl.textContent = 0
+    count  = 0
+}
+
+
+// increment()
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// let lapsCompleted = 0
+
+// function increase (){
+//     lapsCompleted = lapsCompleted +1
+//     console.log(lapsCompleted)
+
+//     lapsCompleted = lapsCompleted +1
+//     console.log(lapsCompleted)
+
+//     lapsCompleted = lapsCompleted +1
+//     console.log(lapsCompleted)
+
+// }
+// increase()
